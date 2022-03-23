052d6ea9e408368cabec742b097c0d3ae3ad8ad8
Ssora <102017076+Ssssssssssora@users.noreply.github.com>
2022-03-23 15:02:17 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+const scene = new ScrollMagic.Scene({
+    duration: 300000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    
+    .setPin(intro)
+    .addTo(controller);
+
+//Video Animation
+let accelamount = 1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 2000;
+});
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = scrollpos;
+}, 33.3);
