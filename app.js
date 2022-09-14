fed9a8c50e49bcaa233f570637162a5af0148129
Ssora <102017076+Ssssssssssora@users.noreply.github.com>
2022-09-14 21:23:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const controller = new ScrollMagic.Controller();
 
 //Scenes
 const scene = new ScrollMagic.Scene({
-    duration: 1000,
+    duration: 2000,
     triggerElement: intro,
     triggerHook: 0
 })
@@ -24,7 +24,7 @@ let scrollpos = 0;
 let delay = 0;
 
 scene.on("update", e => {
-    scrollpos = e.scrollPos / 5;
+    scrollpos = e.scrollPos / 10;
 });
 
 setInterval(() => {
