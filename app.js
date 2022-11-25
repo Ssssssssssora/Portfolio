de3617f700c73e047b2e82d396b1f025359396fe
Ssora <102017076+Ssssssssssora@users.noreply.github.com>
2022-11-25 14:59:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const intro = document.querySelector(".intro");
 const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
 //END SECTION
-const section = document.querySelector("section");
-const end = section.querySelector("h1");
 
 //SCROLLMAGIC
 const controller = new ScrollMagic.Controller();
 
 //Scenes
 const scene = new ScrollMagic.Scene({
-    duration: 3300,
+    duration: 132000,
     triggerElement: intro,
     triggerHook: 0
 })
-    
-    .setPin(intro)
+    .addIndicator();
+    .setPin(intro);
     .addTo(controller);
 
 //Video Animation
-let accelamount = 1;
+let accelamount = 0.1;
 let scrollpos = 0;
-let delay = 1000;
+let delay = 0;
 
 scene.on("update", e => {
-    scrollpos = e.scrollPos;
+    scrollpos = e.scrollPos/1000;
+    console.log(e);
 });
 
-setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
-    console.log(scrollpos, delay);
+//setInterval(() => {
+    //delay += (scrollpos - delay) * accelamount;
+    //console.log(scrollpos, delay);
 
-    video.currentTime = scrollpos;
-}, 33.3);
+    //video.currentTime = scrollpos;
+//}, 33.3);
